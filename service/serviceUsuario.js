913a72bb38d8a213301e0515c67b157ae30471dd
AlezLg <frutel1234@gmail.com>
2025-01-12 19:32:44 -0300

    ave maria

--- service/serviceUsuario.js
+++ service/serviceUsuario.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs';
 import { Usuario } from "../model/ModelUsuarios.js";
 import { generateRefreshToken, generateAccessToken } from "../utils/Tokens.js";
 
